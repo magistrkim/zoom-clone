8a13a27a3aa51caee55a45054ded453affd612ef
Nataliya Kim <magistr1982@gmail.com>
2024-06-19 09:40:19 +0300

    Create Navbar and Sidebar template

--- app/(root)/(home)/layout.tsx
+++ app/(root)/(home)/layout.tsx
@@ -1,16 +1,17 @@
+import Navbar from "@/components/ui/Navbar";
+import Sidebar from "@/components/ui/Sidebar";
 import React, { ReactNode } from "react";
 
 const HomeLayout = ({ children }: { children: ReactNode }) => {
   return (
     <main className="relative">
-      Navbar
+      <Navbar />
       <div className="flex">
-        SideBar
+        <Sidebar />
         <section className="flex flex-1 flex-col min-h-screen px-6 pb-6 pt-28 max-md:pb-14 sm:px-14">
-          <div></div>
+          <div className="w-full">{children}</div>
         </section>
       </div>
-      {children}
     </main>
   );
 };
