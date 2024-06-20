a9cea2904de71f3b9c6b397bd1642cb5708013a0
Nataliya Kim <magistr1982@gmail.com>
2024-06-20 09:29:33 +0300

    Create Sidebar UI

--- components/ui/Sidebar.tsx
+++ components/ui/Sidebar.tsx
@@ -1,13 +1,37 @@
+"use client";
+import { sidebarLinks } from "@/constants";
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
 
 const Sidebar = () => {
+  const pathname = usePathname();
   return (
     <section
       className="sticky left-0 top-0 h-screen w-fit text-white bg-dark-1 
     flex-col justify-between pt-28 p-6 max-sm:hidden lg:w-[264px]"
     >
-      <div className="flex flex-1 flex-col gap-6"></div>
-      Sidebar
+      <div className="flex flex-1 flex-col gap-6">
+        {sidebarLinks.map((link) => {
+          const isActive =
+            pathname === link.route || pathname.startsWith(link.route);
+          return (
+            <Link
+              href={link.route}
+              key={link.label}
+              className={cn(
+                "flex justify-start items-center gap-4 p-4 rounded-lg",
+                {
+                  "bg-blue-1": isActive,
+                }
+              )}
+            >
+              {link.label}
+            </Link>
+          );
+        })}
+      </div>
     </section>
   );
 };
