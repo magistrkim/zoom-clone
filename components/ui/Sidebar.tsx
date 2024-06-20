a5a8ab3393842c41a6b96b8cff9dfdfdc486e9d1
Nataliya Kim <magistr1982@gmail.com>
2024-06-20 09:14:44 +0300

    Create constants folder

--- components/ui/Sidebar.tsx
+++ components/ui/Sidebar.tsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
 
 const Sidebar = () => {
   return (
-    <div>Sidebar</div>
-  )
-}
+    <section
+      className="sticky left-0 top-0 h-screen w-fit text-white bg-dark-1 
+    flex-col justify-between pt-28 p-6 max-sm:hidden lg:w-[264px]"
+    >
+      <div className="flex flex-1 flex-col gap-6"></div>
+      Sidebar
+    </section>
+  );
+};
 
-export default Sidebar
+export default Sidebar;
