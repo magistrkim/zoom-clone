51bf4fd420dba5ebaff379003881389eb846b653
Nataliya Kim <magistr1982@gmail.com>
2024-06-23 10:44:23 +0300

    Install shadcn Sheet component

--- components/ui/MobileNav.tsx
+++ components/ui/MobileNav.tsx
@@ -1,11 +1,39 @@
-import React from 'react'
+import React from "react";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import Image from "next/image";
 
 const MobileNav = () => {
   return (
-    <div className="text-lg font-bold text-white ">
-      MobileNav
+    <div className="w-full max-w-[264px]">
+      <Sheet>
+        <SheetTrigger asChild>
+          <Image
+            src="/icons/hamburger.svg"
+            alt="mobile-menu"
+            width={36}
+            height={36}
+            className="cursor-pointer sm:hidden"
+          />
+        </SheetTrigger>
+        <SheetContent>
+          <SheetHeader>
+            <SheetTitle>Are you absolutely sure?</SheetTitle>
+            <SheetDescription>
+              This action cannot be undone. This will permanently delete your
+              account and remove your data from our servers.
+            </SheetDescription>
+          </SheetHeader>
+        </SheetContent>
+      </Sheet>
     </div>
   );
-}
+};
 
-export default MobileNav
+export default MobileNav;
