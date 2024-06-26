7fbb51453fbe4baf3dbc6277b50b9889261627cc
Nataliya Kim <magistr1982@gmail.com>
2024-06-26 11:29:08 +0300

    Add HomePage UI

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -3,7 +3,19 @@ import React from "react";
 const Home = () => {
   return (
     <section className="flex flex-col gap-10 text-white size-full">
-      <h1 className="text-3xl font-bold">Home</h1>
+      <div className="w-full h-[300px] rounded-[20px] bg-hero bg-cover">
+        <div className="flex flex-col h-full justify-between max-md:px-5 max-md:py-8 lg:p-11">
+          <h2 className="text-base font-normal text-center py-2 max-w-[270px] glassmorphism rounded">
+            Upcoming Meeting at: 12:30 PM
+          </h2>
+          <div className="flex flex-col gap-2">
+            <h1 className="text-4xl font-extrabold lg:text-7xl">11:24 AM</h1>
+            <p className="text-lg font-medium text-sky-1 lg:text-2xl">
+              Wednesday, June 26, 2024
+            </p>
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
