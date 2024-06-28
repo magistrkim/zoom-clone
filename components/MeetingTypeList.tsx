d79997c0c28430ad3cd88d7abfd026d0f8ff8411
Nataliya Kim <magistr1982@gmail.com>
2024-06-28 08:38:43 +0300

    Create MeetingTypeList component

---
+++ components/MeetingTypeList.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import React from "react";
+
+const MeetingTypeList = () => {
+  return <div className="text-white font-bold">MeetingTypeList</div>;
+};
+
+export default MeetingTypeList;
