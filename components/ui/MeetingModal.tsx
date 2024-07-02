bf9cbcf70df726dabe0f1dcfd8249054f14072c3
Nataliya Kim <magistr1982@gmail.com>
2024-07-02 08:56:45 +0300

    Complete Modal UI

--- components/ui/MeetingModal.tsx
+++ components/ui/MeetingModal.tsx
@@ -7,6 +7,9 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
+import Image from "next/image";
+import { cn } from "@/lib/utils";
+import { Button } from "./button";
 
 interface MeetingModalProps {
   image?: string;
@@ -32,16 +35,34 @@ const MeetingModal = ({
   buttonIcon,
 }: MeetingModalProps) => {
   return (
-    <Dialog>
-      <DialogTrigger>Open</DialogTrigger>
-      <DialogContent>
-        <DialogHeader>
-          <DialogTitle>Are you absolutely sure?</DialogTitle>
-          <DialogDescription>
-            This action cannot be undone. This will permanently delete your
-            account and remove your data from our servers.
-          </DialogDescription>
-        </DialogHeader>
+    <Dialog open={isOpen} onOpenChange={onClose}>
+      <DialogContent className="flex flex-col w-full max-w-[520px] 
+        border-none bg-dark-1 gap-6 px-6 py-9 text-white">
+        <div className="flex flex-col gap-6">
+          {image && (
+            <div className="flex justify-center">
+              <Image src={image} alt="image" width={72} height={72} />
+            </div>
+          )}
+          <h1 className={cn("text-3xl font-bold leading-[42px]", className)}>
+            {title}
+          </h1>
+          {children}
+          <Button
+            className="bg-blue-1 focus-visible:ring-0 focus-visible:ring-offset-0"
+            onClick={handleClick}
+          >
+            {buttonIcon && (
+              <Image
+                src={buttonIcon}
+                alt="button icon"
+                width={13}
+                height={13}
+              />
+            )} &nbsp;
+            {buttonText || "Schedule Meeting"}
+          </Button>
+        </div>
       </DialogContent>
     </Dialog>
   );
