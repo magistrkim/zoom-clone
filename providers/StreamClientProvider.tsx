555955cd61abc3c79c335ed1ceb40dd3f65f5110
Nataliya Kim <magistr1982@gmail.com>
2024-07-04 10:57:11 +0300

    Create StreamVideoProvider

---
+++ providers/StreamClientProvider.tsx
@@ -0,0 +1,34 @@
+import { useUser } from "@clerk/nextjs";
+import { StreamVideo, StreamVideoClient } from "@stream-io/video-react-sdk";
+import { ReactNode, useState, useEffect } from "react";
+
+const apiKey = process.env.NEXT_PUBLIC_STREAM_API_KEY;
+// const userId = "user-id";
+// const token = "authentication-token";
+// const user: User = { id: userId };
+
+// const client = new StreamVideoClient({ apiKey, user, token });
+// const call = client.call("default", "my-first-call");
+// call.join({ create: true });
+
+const StreamVideoProvider = ({ children }: { children: ReactNode }) => {
+  const [videoClient, setVideoClient] = useState<StreamVideoClient>();
+  const { user, isLoaded } = useUser();
+
+  useEffect(() => {
+    if (!isLoaded || !user) return;
+    if (!apiKey) throw Error("Stream API Key missing");
+    const client = new StreamVideoClient({
+      apiKey,
+      user: {
+        id: user?.id,
+        name: user?.username || user?.id,
+        image: user?.imageUrl,
+      },
+    });
+  }, [user, isLoaded]);
+
+  return <StreamVideo client={videoClient}></StreamVideo>;
+};
+
+export default StreamVideoProvider;
