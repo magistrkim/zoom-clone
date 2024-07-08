34734ef184e62f0cd291423c06c5b80160e84a96
Nataliya Kim <magistr1982@gmail.com>
2024-07-08 11:57:54 +0300

    Complete MeetingTypeList with a createMeeting

--- components/ui/MeetingTypeList.tsx
+++ components/ui/MeetingTypeList.tsx
@@ -14,8 +14,33 @@ const MeetingTypeList = () => {
   >();
   const { user } = useUser();
   const client = useStreamVideoClient();
-  const createMeeting = () => {
-    if(!client || !user) return;
+  const [values, setValues] = useState({
+    dateTime: new Date(),
+    description: "",
+    link: "",
+  });
+  const [callDetails, setCallDetails] = useState();
+  
+  const createMeeting = async () => {
+    if (!client || !user) return;
+    try {
+      const id = crypto.randomUUID();
+      const call = client.call("default", id);
+      if (!call) throw new Error("Failed to create call");
+      const startsAt =
+        values.dateTime.toISOString() || new Date(Date.now()).toISOString();
+      const description = values.description || "Instant meeting";
+      await call.getOrCreate({
+        data: {
+          starts_at: startsAt,
+          custom: {
+            description,
+          },
+        },
+      });
+    } catch (error) {
+      console.log(error);
+    }
   };
   return (
     <section className="grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4">
