97d52c513b32b6233fe5a8facdf279994bce378d
Nataliya Kim <magistr1982@gmail.com>
2024-07-12 09:38:32 +0300

    Create useGetCallById hook

--- app/(root)/meeting/[id]/page.tsx
+++ app/(root)/meeting/[id]/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import MeetingRoom from "@/components/ui/MeetingRoom";
 import MeetingSetup from "@/components/ui/MeetingSetup";
+import { useGetCallById } from "@/hooks/useGetCallById";
 import { useUser } from "@clerk/nextjs";
 import { StreamCall, StreamTheme } from "@stream-io/video-react-sdk";
 import { useState } from "react";
@@ -10,7 +11,7 @@ const Meeting = ({ params }: { params: { id: string } }) => {
   const [isSetupComplete, setIsSetupComplete] = useState(false);
   return (
     <main className="h-screen w-full">
-      <StreamCall>
+      <StreamCall call={}>
         <StreamTheme>
           {!isSetupComplete ? <MeetingSetup/> : <MeetingRoom/>}
         </StreamTheme>
