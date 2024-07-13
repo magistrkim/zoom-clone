e3e71067470dff6be4d01105c8c80824acd5fe45
Nataliya Kim <magistr1982@gmail.com>
2024-07-13 12:06:55 +0300

    Add changes to MeetingSetup

--- components/ui/MeetingSetup.tsx
+++ components/ui/MeetingSetup.tsx
@@ -1,12 +1,25 @@
-import { VideoPreview } from "@stream-io/video-react-sdk";
-import React from "react";
+"use client";
+
+import { useCall, VideoPreview } from "@stream-io/video-react-sdk";
+import React, { useState, useEffect } from "react";
 
 const MeetingSetup = () => {
-  
+  const [isMicCamToogledOn, setisMicCamToogledOn] = useState(false);
+  const call = useCall();
+  useEffect(() => {
+    if (isMicCamToogledOn) {
+      call?.camera.disable();
+      call?.microphone.disable();
+    } else {
+      call?.camera.enable();
+      call?.microphone.enable();
+    }
+  }, [isMicCamToogledOn, call?.camera, call?.microphone]);
+
   return (
     <div className="text-white flex w-full h-screen justify-center items-center gap-3 flex-col">
       <h1 className="text-2xl font-bold">Setup</h1>
-      <VideoPreview/>
+      <VideoPreview />
     </div>
   );
 };
