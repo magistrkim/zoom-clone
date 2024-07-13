5fe4593de7b5001ecbdfd20cfa8b144558d733d9
Nataliya Kim <magistr1982@gmail.com>
2024-07-13 11:52:41 +0300

    Complete MeetingSetup component

--- components/ui/MeetingSetup.tsx
+++ components/ui/MeetingSetup.tsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import { VideoPreview } from "@stream-io/video-react-sdk";
+import React from "react";
 
 const MeetingSetup = () => {
+  
   return (
-    <div className='text-white font-semibold'>MeetingSetup</div>
-  )
-}
+    <div className="text-white flex w-full h-screen justify-center items-center gap-3 flex-col">
+      <h1 className="text-2xl font-bold">Setup</h1>
+      <VideoPreview/>
+    </div>
+  );
+};
 
-export default MeetingSetup
+export default MeetingSetup;
