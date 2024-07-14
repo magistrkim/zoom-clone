4e2d96732d0a9cab0f20fdbe336a275d8edbfe1f
Nataliya Kim <magistr1982@gmail.com>
2024-07-14 10:55:02 +0300

    Complete Meeting Setup

--- components/ui/MeetingSetup.tsx
+++ components/ui/MeetingSetup.tsx
@@ -6,6 +6,9 @@ import React, { useState, useEffect } from "react";
 const MeetingSetup = () => {
   const [isMicCamToogledOn, setisMicCamToogledOn] = useState(false);
   const call = useCall();
+  if (!call) {
+    throw new Error("usecall must be used within StreamCall component");
+  }
   useEffect(() => {
     if (isMicCamToogledOn) {
       call?.camera.disable();
@@ -20,6 +23,19 @@ const MeetingSetup = () => {
     <div className="text-white flex w-full h-screen justify-center items-center gap-3 flex-col">
       <h1 className="text-2xl font-bold">Setup</h1>
       <VideoPreview />
+      <div className="flex h-16 gap-3 justify-center items-center">
+        <label
+          htmlFor=""
+          className="flex font-medium gap-2 justify-center items-center"
+        >
+          <input
+            type="checkbox"
+            checked={isMicCamToogledOn}
+            onChange={(event) => setisMicCamToogledOn(event.target.checked)}
+          />
+          Join with mic and camera off
+        </label>
+      </div>
     </div>
   );
 };
