758bbb21a439367192b830b0225dcb375ea09882
Nataliya Kim <magistr1982@gmail.com>
2024-07-16 11:07:51 +0300

    Add MeetingRoom component

--- components/ui/MeetingRoom.tsx
+++ components/ui/MeetingRoom.tsx
@@ -1,7 +1,15 @@
-import React from 'react'
+import React from "react";
 
 const MeetingRoom = () => {
-  return <div className="text-white font-semibold">MeetingRoom</div>;
-}
+  return (
+    <section className="relative h-screen w-full pt-4 overfow-hidden text-white">
+      <div className="relative flex size-full items-center justify-center">
+        <div className="flex size-full max-w-[1000px] items-center">
+          
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default MeetingRoom
+export default MeetingRoom;
