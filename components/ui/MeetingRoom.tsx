a37d165d2aff927c9800b8b8d765bd065fd4ebde
Nataliya Kim <magistr1982@gmail.com>
2024-07-16 11:22:00 +0300

    Add CallLayout to MeetingRoom

--- components/ui/MeetingRoom.tsx
+++ components/ui/MeetingRoom.tsx
@@ -1,11 +1,26 @@
-import React from "react";
+import { PaginatedGridLayout, SpeakerLayout } from "@stream-io/video-react-sdk";
+import React, { useState } from "react";
+
+type CallLayoutType = "grid" | "speaker-left" | "speaker-right";
 
 const MeetingRoom = () => {
+  const [layout, setLayout] = useState<CallLayoutType>("speaker-left");
+
+  const CallLayout = () => {
+    switch (layout) {
+      case "grid":
+        return <PaginatedGridLayout />;
+      case "speaker-right":
+        return <SpeakerLayout participantsBarPosition="left" />;
+      default:
+        return <SpeakerLayout participantsBarPosition="right" />;
+    }
+  };
   return (
     <section className="relative h-screen w-full pt-4 overfow-hidden text-white">
       <div className="relative flex size-full items-center justify-center">
         <div className="flex size-full max-w-[1000px] items-center">
-          
+          <CallLayout />
         </div>
       </div>
     </section>
